9efbaf47f1e717ee7f33327c234770b38b4b65f4
Valeria Kononenko <divergence082@gmail.com>
2020-08-18 19:21:52 +0300

    Value as struct

--- pkg/field.go
+++ pkg/field.go
@@ -1,130 +1,15 @@
 package main
 
 import (
-  "encoding/json"
   "fmt"
   "github.com/grafana/grafana-plugin-sdk-go/backend"
   "github.com/grafana/grafana-plugin-sdk-go/data"
   "math"
-  "reflect"
-  "strconv"
-  "strings"
-  "time"
 )
 
 var dateLayout = "2006-01-02"
 var dateTimeLayout = dateLayout + " 15:04:05"
 
-// TODO use (value, struct) to return result
-func parseValue(valueType string, value interface{}, fieldName string) (interface{}, *data.Field, float64, error)  {
-  parseFloatValue := func(value interface{}) (*float64, *data.Field, float64, error)  {
-    if value != nil {
-	  fv := reflect.ValueOf(value).Float()
-	  return &fv, data.NewField(fieldName, nil, []*float64{&fv}), fv, nil
-	} else {
-	  return nil, data.NewField(fieldName, nil, []*float64{nil}), math.NaN(), nil
-	}
-  }
-
-  parseStringValue := func(value interface{}) (*string, *data.Field, float64, error)  {
-    if value != nil {
-	  sv := reflect.ValueOf(value).String()
-	  return &sv, data.NewField(fieldName, nil, []*string{&sv}), math.NaN(), nil
-	} else {
-	  return nil, data.NewField(fieldName, nil, []*string{nil}), math.NaN(), nil
-	}
-  }
-
-  parseUInt64Value := func(value interface{}) (*uint64, *data.Field, float64, error)  {
-    if value != nil {
-	  ui64v, err := strconv.ParseUint(fmt.Sprintf("%v", value), 10, 64)
-	  if err != nil {
-		return nil, nil, math.NaN(), err
-	  } else {
-		return &ui64v, data.NewField(fieldName, nil, []*uint64{&ui64v}), float64(ui64v), nil
-	  }
-	} else {
-	  return nil, data.NewField(fieldName, nil, []*uint64{nil}), math.NaN(), nil
-	}
-  }
-
-  parseInt64Value := func(value interface{}) (*int64, *data.Field, float64, error)  {
-    if value != nil {
-	  i64v, err := strconv.ParseInt(fmt.Sprintf("%v", value), 10, 64)
-	  if err != nil {
-		return nil, nil, math.NaN(), err
-	  } else {
-		return &i64v, data.NewField(fieldName, nil, []*int64{&i64v}), float64(i64v), nil
-	  }
-	} else {
-	  return nil, data.NewField(fieldName, nil, []*int64{nil}), math.NaN(), nil
-	}
-  }
-
-  parseTime := func(value interface{}, layout string) (*time.Time, *data.Field, float64, error)  {
-	strValue := fmt.Sprintf("%v", value)
-	t, err := time.Parse(layout, strValue)
-
-	if err == nil {
-	  return &t, data.NewField(fieldName, nil, []*time.Time{&t}), float64(t.Unix()), nil
-	} else {
-	  i64v, err := strconv.ParseInt(fmt.Sprintf("%v", value), 10, 64)
-
-	  if err == nil {
-		timeValue := time.Unix(i64v, i64v)
-		return &timeValue, data.NewField(fieldName, nil, []*time.Time{&timeValue}), float64(i64v), nil
-	  } else {
-		date, err := time.Parse(layout, strValue)
-
-		if err == nil {
-		  return &date, data.NewField(fieldName, nil, []*time.Time{&date}), float64(date.Unix()), nil
-		}
-	  }
-	}
-
-	return nil, nil, math.NaN(), err
-  }
-
-  if strings.HasPrefix(valueType, "LowCardinality") {
-    return parseValue(strings.TrimSuffix(strings.TrimPrefix(valueType,"LowCardinality("), ")"), value, fieldName)
-  } else if strings.HasPrefix(valueType, "Nullable") {
-	return parseValue(strings.TrimSuffix(strings.TrimPrefix(valueType,"Nullable("), ")"), value, fieldName)
-  } else {
-	switch valueType {
-	case "UInt8", "UInt16", "UInt32", "Int8", "Int16", "Int32", "Float32", "Float64":
-	  return parseFloatValue(value)
-	case "String", "UUID":
-	  return parseStringValue(value)
-	case "UInt64":
-	  return parseUInt64Value(value)
-	case "Int64":
-	  return parseInt64Value(value)
-	case "Date":
-	  return parseTime(value, dateLayout)
-	case "DateTime":
-	  return parseTime(value, dateTimeLayout)
-	default:
-	  if strings.HasPrefix(valueType, "Decimal") {
-		return parseFloatValue(value)
-	  } else if strings.HasPrefix(valueType, "FixedString") || strings.HasPrefix(valueType, "Enum") {
-		return parseStringValue(value)
-	  } else {
-	    backend.Logger.Warn(
-	      fmt.Sprintf("Value [%v] has compound type [%v] and will be returned as string", value, valueType))
-
-	    byteValue, err := json.Marshal(value)
-		if err != nil {
-		  backend.Logger.Error(fmt.Sprintf(
-			"Unable to append value of unknown type %v because of json encoding problem: %s",
-			reflect.TypeOf(value), err))
-		  return nil, nil, math.NaN(), err
-		}
-
-		return parseStringValue(string(byteValue))
-	  }
-	}
-  }
-}
 
 func NewField(name string, fieldType string) *ClickHouseField {
 	return &ClickHouseField{
@@ -144,27 +29,23 @@ type ClickHouseField struct {
 }
 
 func (f *ClickHouseField) Append(value interface{}) {
-	v, field, fv, err := parseValue(f.Type, value, f.Name)
-	if err != nil {
-	  backend.Logger.Warn(
-	    fmt.Sprintf("Value [%v / %v] wouln't be added to Field, because of the error: %v", value, f.Type, err))
-	} else if field == nil {
-	  backend.Logger.Warn(
-		fmt.Sprintf("Value [%v / %v] wouln't be added to Field, because field is null", value, f.Type))
+	v := ParseValue(f.Type, value, f.Name, false)
+	if v == nil {
+	  backend.Logger.Warn(fmt.Sprintf("Value [%v / %v] wouln't be added to Field", value, f.Type))
 	} else {
 	  if f.Field == nil {
-	    f.Field = field
-	  } else {
-	    f.Field.Append(v)
+	    f.Field = v.Field
 	  }
 
-	  if fv != math.NaN() {
-		if math.IsNaN(f.Min) || fv < f.Min {
-		  f.Min = fv
+	  f.Field.Append(v.Val)
+
+	  if !math.IsNaN(v.Float) {
+		if v.Float < f.Min {
+		  f.Min = v.Float
 		}
 
-		if math.IsNaN(f.Max) || fv > f.Max {
-		  f.Max = fv
+		if v.Float > f.Max {
+		  f.Max = v.Float
 		}
 	  }
 	}
---
+++ pkg/value.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/grafana/grafana-plugin-sdk-go/backend"
+  "github.com/grafana/grafana-plugin-sdk-go/data"
+  "math"
+  "reflect"
+  "strconv"
+  "strings"
+  "time"
+)
+
+func parseFloatValue(value interface{}, fieldName string, nullable bool) *Value   {
+
+  if value != nil {
+    fv := reflect.ValueOf(value).Float()
+
+    if nullable {
+	  return &Value{
+		Val: &fv,
+		Field: data.NewField(fieldName, nil, []*float64{}),
+		Float: fv,
+	  }
+	} else {
+	  return &Value{
+		Val: fv,
+		Field: data.NewField(fieldName, nil, []float64{}),
+		Float: fv,
+	  }
+	}
+  } else if nullable {
+	return &Value{
+	  Val: nil,
+	  Field: data.NewField(fieldName, nil, []*float64{}),
+	  Float: math.NaN(),
+	}
+  }
+
+  return nil
+}
+
+func parseStringValue(value interface{}, fieldName string, nullable bool) *Value   {
+  if value != nil {
+	str := reflect.ValueOf(value).String()
+
+	if nullable {
+	  return &Value{
+		Val: &str,
+		Field: data.NewField(fieldName, nil, []*string{}),
+		Float: math.NaN(),
+	  }
+	} else {
+	  return &Value{
+		Val: str,
+		Field: data.NewField(fieldName, nil, []string{}),
+		Float: math.NaN(),
+	  }
+	}
+  } else if nullable {
+	return &Value{
+	  Val: nil,
+	  Field: data.NewField(fieldName, nil, []*string{}),
+	  Float: math.NaN(),
+	}
+  }
+
+  return nil
+}
+
+func parseUInt64Value(value interface{}, fieldName string, nullable bool) *Value  {
+
+  if value != nil {
+	ui64v, err := strconv.ParseUint(fmt.Sprintf("%v", value), 10, 64)
+
+	if err == nil {
+	  if nullable {
+		return &Value{
+		  Val:   &ui64v,
+		  Field: data.NewField(fieldName, nil, []*uint64{}),
+		  Float: float64(ui64v),
+		}
+	  } else {
+		return &Value{
+		  Val:   ui64v,
+		  Field: data.NewField(fieldName, nil, []uint64{}),
+		  Float: float64(ui64v),
+		}
+	  }
+	}
+  } else if nullable {
+	return &Value{
+	  Val: nil,
+	  Field: data.NewField(fieldName, nil, []*uint64{}),
+	  Float: math.NaN(),
+	}
+  }
+
+  return nil
+}
+
+func parseInt64Value(value interface{}, fieldName string, nullable bool) *Value  {
+
+  if value != nil {
+	i64v, err := strconv.ParseInt(fmt.Sprintf("%v", value), 10, 64)
+
+	if err == nil {
+	  if nullable {
+		return &Value{
+		  Val:   &i64v,
+		  Field: data.NewField(fieldName, nil, []*int64{}),
+		  Float: float64(i64v),
+		}
+	  } else {
+		return &Value{
+		  Val:   i64v,
+		  Field: data.NewField(fieldName, nil, []int64{}),
+		  Float: float64(i64v),
+		}
+	  }
+	}
+  } else if nullable {
+	return &Value{
+	  Val: nil,
+	  Field: data.NewField(fieldName, nil, []*int64{}),
+	  Float: math.NaN(),
+	}
+  }
+
+  return nil
+}
+
+func parseTimeValue(value interface{}, fieldName string, nullable bool, layout string) *Value  {
+
+  toNullableValue := func(t time.Time) *Value {
+	return &Value{
+	  Val:   &t,
+	  Field: data.NewField(fieldName, nil, []*time.Time{}),
+	  Float: float64(t.Unix()),
+	}
+  }
+
+  toValue := func(t time.Time) *Value {
+	return &Value{
+	  Val:   t,
+	  Field: data.NewField(fieldName, nil, []time.Time{}),
+	  Float: float64(t.Unix()),
+	}
+  }
+
+  if value != nil {
+	strValue := fmt.Sprintf("%v", value)
+	t, err := time.Parse(layout, strValue)
+
+	if err == nil {
+	  if nullable {
+		return toNullableValue(t)
+	  } else {
+		return toValue(t)
+	  }
+	} else {
+	  i64v, err := strconv.ParseInt(strValue, 10, 64)
+
+	  if err == nil {
+		timeValue := time.Unix(i64v, i64v)
+
+		if nullable {
+		  return toNullableValue(timeValue)
+		} else {
+		  return toValue(timeValue)
+		}
+	  }
+	}
+  } else if nullable {
+	return &Value{
+	  Val: nil,
+	  Field: data.NewField(fieldName, nil, []*time.Time{}),
+	  Float: math.NaN(),
+	}
+  }
+
+  return nil
+}
+
+func ParseValue(valueType string, value interface{}, fieldName string, nullable bool) *Value  {
+
+  if strings.HasPrefix(valueType, "LowCardinality") {
+	return ParseValue(strings.TrimSuffix(strings.TrimPrefix(valueType,"LowCardinality("), ")"),
+	  value, fieldName, nullable)
+  } else if strings.HasPrefix(valueType, "Nullable") {
+	return ParseValue(strings.TrimSuffix(strings.TrimPrefix(valueType,"Nullable("), ")"),
+	  value, fieldName, true)
+  } else {
+	switch valueType {
+	case "UInt8", "UInt16", "UInt32", "Int8", "Int16", "Int32", "Float32", "Float64":
+	  return parseFloatValue(value, fieldName, nullable)
+	case "String", "UUID":
+	  return parseStringValue(value, fieldName, nullable)
+	case "UInt64":
+	  return parseUInt64Value(value, fieldName, nullable)
+	case "Int64":
+	  return parseInt64Value(value, fieldName, nullable)
+	case "Date":
+	  return parseTimeValue(value, fieldName, nullable, dateLayout)
+	case "DateTime":
+	  return parseTimeValue(value, fieldName, nullable, dateTimeLayout)
+	default:
+	  if strings.HasPrefix(valueType, "Decimal") {
+		return parseFloatValue(value, fieldName, nullable)
+	  } else if strings.HasPrefix(valueType, "FixedString") || strings.HasPrefix(valueType, "Enum") {
+		return parseStringValue(value, fieldName, nullable)
+	  } else {
+		backend.Logger.Warn(
+		  fmt.Sprintf("Value [%v] has compound type [%v] and will be returned as string", value, valueType))
+
+		byteValue, err := json.Marshal(value)
+		if err != nil {
+		  backend.Logger.Warn(fmt.Sprintf(
+			"Unable to append value of unknown type %v because of json encoding problem: %s",
+			reflect.TypeOf(value), err))
+		  return nil
+		}
+
+		return parseStringValue(string(byteValue), fieldName, nullable)
+	  }
+	}
+  }
+}
+
+type Value struct {
+  Val interface{}
+  Field *data.Field
+  Float float64
+}
