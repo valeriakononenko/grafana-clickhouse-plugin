6b0c18f29faa5c8cbc46cd31ee8374434768bd24
Valeria Kononenko <divergence082@gmail.com>
2020-09-24 12:23:20 +0300

    query script

--- scripts/query.sh
+++ scripts/query.sh
@@ -1,7 +1,9 @@
 
 
-USER=$1
-PASSWORD=$2
-QUERY=$3
+HOST=$1
+PORT=$2
+USER=$3
+PASSWORD=$4
+QUERY=$5
 
-echo $QUERY | curl -H "X-ClickHouse-User: $USER" -H "X-ClickHouse-Key: $PASSWORD" "http://localhost:8123/" --data-binary @-
+echo $QUERY | curl -H "X-ClickHouse-User: $USER" -H "X-ClickHouse-Key: $PASSWORD" "http://$HOST:$PORT/" --data-binary @-
